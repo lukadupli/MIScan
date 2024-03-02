04a1ffc718e172b5db716ad74e26eddb54d78967
Luka D <luka.duplancic@gmail.com>
2024-03-02 17:43:30 +0100

    oke

--- lib/corner_showcase.dart
+++ lib/corner_showcase.dart
@@ -14,7 +14,6 @@ class ShowcasePainter extends CustomPainter{
     final src = Offset(position.value.dx - imageSegmentSize.width / 2, position.value.dy - imageSegmentSize.height / 2) & imageSegmentSize;
     canvas.drawImageRect(image, src, Offset.zero & size, Paint());
   }
-  
 
   @override
   bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
